99d574461b561b28f2c0a741da053e7b9901580b
coffeebeforebed <bradmgallen@gmail.com>
2016-08-07 01:01:46 -0700

    initial commit

---
+++ index.php
@@ -0,0 +1,4 @@
+<?php include 'templates/head.php';?>
+<?php include 'includes/bellyband.php';?>
+<iframe id='mechanicalreproduction' src="https://docs.google.com/document/d/1FxnoWKBfwY7Sb4JUfRFMBjuM-2c-uNWLJfrxK_rQkWo/edit?usp=sharing"></iframe>
+<?php include 'templates/foot.php';?>
